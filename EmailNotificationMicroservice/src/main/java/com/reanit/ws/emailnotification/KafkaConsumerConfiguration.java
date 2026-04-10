4043a956d9670a16c5a3e35b1c192d9bcec9799a
HORKimhab <41143106+HORKimhab@users.noreply.github.com>
2026-04-10 15:28:57 +0700

    lesson: kafka listender container factory - 10

--- EmailNotificationMicroservice/src/main/java/com/reanit/ws/emailnotification/KafkaConsumerConfiguration.java
+++ EmailNotificationMicroservice/src/main/java/com/reanit/ws/emailnotification/KafkaConsumerConfiguration.java
@@ -8,6 +8,7 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.support.serializer.JacksonJsonDeserializer;
@@ -34,5 +35,13 @@ public class KafkaConsumerConfiguration {
         config.put(ConsumerConfig.GROUP_ID_CONFIG, consumerGroupId);
 
         return new DefaultKafkaConsumerFactory<>(config);  
+    }
+
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory (ConsumerFactory<String, Object> consumerFactory){
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+
+        return factory;
     }
 }
