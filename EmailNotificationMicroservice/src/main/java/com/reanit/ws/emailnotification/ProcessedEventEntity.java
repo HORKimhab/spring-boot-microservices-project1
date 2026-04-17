d88bffd0ff27b3312f70de2ba7ad4cd1acbadc2f
HORKimhab <41143106+HORKimhab@users.noreply.github.com>
2026-04-17 14:25:57 +0700

    lesson: creating JPA Entity -14.06

---
+++ EmailNotificationMicroservice/src/main/java/com/reanit/ws/emailnotification/ProcessedEventEntity.java
@@ -0,0 +1,56 @@
+package com.reanit.ws.emailnotification;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+public class ProcessedEventEntity implements Serializable {
+
+    private static final long serialVersionUID = 7428915631042789156L;
+
+    @Id
+    @GeneratedValue
+    private long id; 
+
+    @Column(nullable=false, unique=true)
+    private String messageId; 
+
+    @Column(nullable=false)
+    private String productId; 
+
+    public ProcessedEventEntity(){
+    }
+
+    public ProcessedEventEntity(long id, String messageId, String productId){
+        this.id = id; 
+        this.messageId = messageId; 
+        this.productId = productId; 
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(String messageId) {
+        this.messageId = messageId;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+}
