68f53ff6a89e3d00ea9fea823b0da942dcc0eabb
HORKimhab <41143106+HORKimhab@users.noreply.github.com>
2026-05-04 11:25:51 +0700

    lesson: the setup() and tearDown() methods 17.08

--- ProductsMicroservice/src/test/java/com/reanit/ws/products/ProductsServiceIntegrationTest.java
+++ ProductsMicroservice/src/test/java/com/reanit/ws/products/ProductsServiceIntegrationTest.java
@@ -2,21 +2,33 @@ package com.reanit.ws.products;
 
 import java.math.BigDecimal;
 import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.env.Environment;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.KafkaMessageListenerContainer;
+import org.springframework.kafka.listener.MessageListener;
+import org.springframework.kafka.listener.adapter.KafkaMessageHandlerMethodFactory;
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JacksonJsonDeserializer;
 import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.ContainerTestUtils;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
+import com.reanit.ws.core.ProductCreatedEvent;
 // import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.reanit.ws.products.rest.CreateProductRestModel;
 import com.reanit.ws.products.service.ProductService;
@@ -63,6 +75,20 @@ public class ProductsServiceIntegrationTest {
     @Autowired
     Environment environment; 
 
+    private KafkaMessageListenerContainer<String, ProductCreatedEvent> container; 
+    private BlockingQueue<ConsumerRecord<String, ProductCreatedEvent>> records; 
+
+    @BeforeAll 
+    void setUp(){
+        DefaultKafkaConsumerFactory<String, Object> consumerFactory = new DefaultKafkaConsumerFactory<>(getConsumerProperties()); 
+        ContainerProperties containerProperties = new ContainerProperties(environment.getProperty("product-created-events-topic-name"));
+        container = new KafkaMessageListenerContainer<>(consumerFactory, containerProperties);
+        records = new LinkedBlockingDeque<>();
+        container.setupMessageListener((MessageListener<String, ProductCreatedEvent>) records::add);
+
+        ContainerTestUtils.waitForAssignment(containerProperties, embeddedKafkaBroker.getPartitionsPerTopic());
+    }
+
     @Test
     void testCreateProduct_whenGivenValidProductDetails_successfullSendsKafakaMessage() throws Exception{
 
@@ -92,6 +118,11 @@ public class ProductsServiceIntegrationTest {
             JacksonJsonDeserializer.TRUSTED_PACKAGES, environment.getProperty("spring.kafka.consumer.properties.spring.json.trusted.packages"),
             ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, environment.getProperty("spring.kafka.consumer.auto-offset-reset")
         ); 
+    }
+
+    @AfterAll
+    void tearDown(){
+        container.stop(); 
     }
 
 }
