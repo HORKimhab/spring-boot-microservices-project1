4bca13efdb920de886150fc2eba700414e0bf0c9
HORKimhab <41143106+HORKimhab@users.noreply.github.com>
2026-05-04 14:22:23 +0700

    lesson: the 'arrange sction - 18.03

--- EmailNotificationMicroservice/src/test/java/com/reanit/ws/emailnotification/ProductCreatedEventHandlerIntegrationTest.java
+++ EmailNotificationMicroservice/src/test/java/com/reanit/ws/emailnotification/ProductCreatedEventHandlerIntegrationTest.java
@@ -1,10 +1,38 @@
 package com.reanit.ws.emailnotification;
 
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+
+import com.reanit.ws.core.ProductCreatedEvent;
 
 @EmbeddedKafka
 @SpringBootTest(properties="spring.kafka.consumer.bootstrap-servers=${spring.embedded.kafka.brokers}")
 public class ProductCreatedEventHandlerIntegrationTest {
+
+
+    @Test
+    public void testProductCreatedEventHandler_OnProductCreated_HandlesEvent(){
+
+         // Arrange 
+        ProductCreatedEvent productCreatedEvent = new ProductCreatedEvent(); 
+        productCreatedEvent.setPrice(new BigDecimal(10));
+        productCreatedEvent.setProductId(UUID.randomUUID().toString());
+        productCreatedEvent.setTitle("Test product - write test case");
+
+        String messageId = UUID.randomUUID().toString(); 
+        String messageKey = productCreatedEvent.getProductId(); 
+
+        ProducerRecord<String, ProductCreatedEvent> record = new ProducerRecord<>("product-created-events-topic", messageKey, productCreatedEvent);
+
+        record.headers().add("messageId", messageId.getBytes());
+        record.headers().add(KafkaHeaders.RECEIVED_KEY, messageKey.getBytes());
+
+    }
 
 }
