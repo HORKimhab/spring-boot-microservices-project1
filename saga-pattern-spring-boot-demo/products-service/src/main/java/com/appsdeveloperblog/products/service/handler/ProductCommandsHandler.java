91d757fb00dcd2c8b8e4bd512fd05f136b51b3b2
HORKimhab <41143106+HORKimhab@users.noreply.github.com>
2026-05-07 15:12:08 +0700

    lesson: reserve product in stock - business logic - 19.13

--- saga-pattern-spring-boot-demo/products-service/src/main/java/com/appsdeveloperblog/products/service/handler/ProductCommandsHandler.java
+++ saga-pattern-spring-boot-demo/products-service/src/main/java/com/appsdeveloperblog/products/service/handler/ProductCommandsHandler.java
@@ -1,17 +1,36 @@
 package com.appsdeveloperblog.products.service.handler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import com.appsdeveloperblog.core.dto.Product;
 import com.appsdeveloperblog.core.dto.commands.ReserveProductCommand;
+import com.appsdeveloperblog.products.service.ProductService;
 
 @Component
 @KafkaListener(topics="${products.commands.topic.name}")
 public class ProductCommandsHandler {
 
+    private final ProductService productService; 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public ProductCommandsHandler(ProductService productService) {
+        this.productService = productService;
+    }
+
     @KafkaListener
     public void handleCommand(@Payload ReserveProductCommand command){
-        
+
+        try {
+            Product desiredProduct = new Product(command.getProductId(), command.getProductQuantity());
+            Product reservedProduct = productService.reserve(desiredProduct, command.getOrderId());
+        } catch (Exception e) {
+            // throw  new RuntimeErrorException(e);
+            logger.error(e.getLocalizedMessage(), e);
+        }
+      
     }
 }
