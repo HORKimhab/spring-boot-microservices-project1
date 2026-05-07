312520b710c21fdf46c5005a32424a3c3547095e
HORKimhab <41143106+HORKimhab@users.noreply.github.com>
2026-05-07 13:00:58 +0700

    lesson: send reserveProductCommand - 19.09

--- saga-pattern-spring-boot-demo/orders-service/src/main/java/com/appsdeveloperblog/orders/config/KafkaConfig.java
+++ saga-pattern-spring-boot-demo/orders-service/src/main/java/com/appsdeveloperblog/orders/config/KafkaConfig.java
@@ -14,6 +14,9 @@ public class KafkaConfig {
     @Value("${orders.events.topic.name}")
     private String ordersEventsTopicName;
 
+    @Value("${products.commands.topic.name}")
+    private String productsCommandsTopicName;
+
     private final static Integer TOPIC_REPLICATION_FACTOR=3;
     private final static Integer TOPIC_PARTITIONS=3;
 
@@ -25,6 +28,14 @@ public class KafkaConfig {
     @Bean
     NewTopic createOrdersEventsTopic() {
         return TopicBuilder.name(ordersEventsTopicName)
+                .partitions(TOPIC_PARTITIONS)
+                .replicas(TOPIC_REPLICATION_FACTOR)
+                .build();
+    }
+
+    @Bean
+    NewTopic createProductsCommandsTopic() {
+        return TopicBuilder.name(productsCommandsTopicName)
                 .partitions(TOPIC_PARTITIONS)
                 .replicas(TOPIC_REPLICATION_FACTOR)
                 .build();
--- saga-pattern-spring-boot-demo/orders-service/src/main/java/com/appsdeveloperblog/orders/saga/OrderSaga.java
+++ saga-pattern-spring-boot-demo/orders-service/src/main/java/com/appsdeveloperblog/orders/saga/OrderSaga.java
@@ -1,19 +1,36 @@
 package com.appsdeveloperblog.orders.saga;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import com.appsdeveloperblog.core.dto.commands.ReserveProductCommand;
 import com.appsdeveloperblog.core.dto.events.OrderCreatedEvent;
 
 @Component
 @KafkaListener(topics={"${orders.events.topic.name}"})
 public class OrderSaga {
 
+    @Value("${products.commands.topic.name}")
+    private String productsCommandsTopicName;
+    private final KafkaTemplate<String, Object> kafkaTemplate; 
+
+    public OrderSaga(KafkaTemplate<String, Object> kafkaTemplate){
+        this.kafkaTemplate = kafkaTemplate; 
+    }
+
     @KafkaHandler
     public void handleEvent(@Payload OrderCreatedEvent event){
+        ReserveProductCommand command = new ReserveProductCommand(
+            event.getProductId(), 
+            event.getProductQuantity(), 
+            event.getOrderId()
+        ); 
 
+        kafkaTemplate.send(productsCommandsTopicName, command);
     }
 
 }
