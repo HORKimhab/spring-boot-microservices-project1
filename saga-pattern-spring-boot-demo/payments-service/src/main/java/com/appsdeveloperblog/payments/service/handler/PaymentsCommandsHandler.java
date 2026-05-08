82a798084684c038a07d119e3bdf3610028e751e
HORKimhab <41143106+HORKimhab@users.noreply.github.com>
2026-05-08 15:58:38 +0700

    lesson: handle process payment command - business logic - 19.19

--- saga-pattern-spring-boot-demo/payments-service/src/main/java/com/appsdeveloperblog/payments/service/handler/PaymentsCommandsHandler.java
+++ saga-pattern-spring-boot-demo/payments-service/src/main/java/com/appsdeveloperblog/payments/service/handler/PaymentsCommandsHandler.java
@@ -1,18 +1,42 @@
 package com.appsdeveloperblog.payments.service.handler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import com.appsdeveloperblog.core.dto.Payment;
 import com.appsdeveloperblog.core.dto.commands.ProcessPaymentCommand;
+import com.appsdeveloperblog.core.exceptions.CreditCardProcessorUnavailableException;
+import com.appsdeveloperblog.payments.service.PaymentService;
 
 @Component
 @KafkaListener(topics="${payments.commands.topic.name}")
 public class PaymentsCommandsHandler {
 
+    private final PaymentService paymentService; 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public PaymentsCommandsHandler(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
     @KafkaHandler
     public void handleCommand(@Payload ProcessPaymentCommand command){
-        
+
+        try {
+            Payment payment = new Payment(
+                command.getOrderId(), 
+                command.getProductId(), 
+                command.getProductPrice(), 
+                command.getProductQuantity()
+            );
+    
+            Payment processedPayment = paymentService.process(payment);
+        } catch (CreditCardProcessorUnavailableException e) {
+            logger.error(e.getLocalizedMessage(), e);
+        }
     }
 }
