a58ce6fd0962ed4b6e603d5777b0048e2cb33a3c
HORKimhab <41143106+HORKimhab@users.noreply.github.com>
2026-05-11 16:07:49 +0700

    lesson: publish ProductReservationCancelledEvent - 20.06

--- saga-pattern-spring-boot-demo/products-service/src/main/java/com/appsdeveloperblog/products/service/handler/ProductCommandsHandler.java
+++ saga-pattern-spring-boot-demo/products-service/src/main/java/com/appsdeveloperblog/products/service/handler/ProductCommandsHandler.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Component;
 
 import com.appsdeveloperblog.core.dto.Product;
 import com.appsdeveloperblog.core.dto.commands.CancelProductReservationCommand;
+import com.appsdeveloperblog.core.dto.commands.ProductReservationCancelledEvent;
 import com.appsdeveloperblog.core.dto.commands.ReserveProductCommand;
 import com.appsdeveloperblog.core.dto.events.ProductReservationFailedEvent;
 import com.appsdeveloperblog.core.dto.events.ProductReservedEvent;
@@ -64,5 +65,10 @@ public class ProductCommandsHandler {
     public void handleCommand(@Payload CancelProductReservationCommand command){
         Product productToCancel = new Product(command.getProductId(), command.getProductQuantity());
         productService.cancelReservation(productToCancel, command.getOrderId());
+
+        ProductReservationCancelledEvent productReservationCancelledEvent = 
+            new ProductReservationCancelledEvent(command.getProductId(), command.getOrderId());
+
+        kafkaTemplate.send(productEventsTopicName, productReservationCancelledEvent);
     }
 }
